d54485ffd1473e3b672d1aded5102739b509a382
xavelo <xavelo@gmail.com>
2025-09-05 15:48:07 +0200

    v1

--- src/main/java/com/xavelo/template/port/in/ListUsersUseCase.java
+++
@@ -1,6 +0,0 @@
-package com.xavelo.template.port.in;
-
-public interface ListUsersUseCase {
-    Long listUsers();
-
-}
--- src/main/java/com/xavelo/template/render/api/adapter/in/http/UserController.java
+++ src/main/java/com/xavelo/template/render/api/adapter/in/http/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v1")
 public class UserController {
 
     private static final Logger logger = LogManager.getLogger(UserController.class);
