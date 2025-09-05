ea6808625508e0414fd878074d0b2bcf4b513353
xavelo <xavelo@gmail.com>
2025-09-05 13:20:43 +0200

    refactor and cleanup

--- src/main/java/com/xavelo/template/TemplateApiRenderApplication.java
+++ src/main/java/com/xavelo/template/TemplateApiRenderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApiApplication {
+public class TemplateApiRenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApiApplication.class, args);
+		SpringApplication.run(TemplateApiRenderApplication.class, args);
 	}
 
 }
--- src/test/java/com/xavelo/template/TemplateApiRenderApplicationTests.java
+++ src/test/java/com/xavelo/template/TemplateApiRenderApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class TemplateApiApplicationTests {
+class TemplateApiRenderApplicationTests {
 
 	@Autowired
 	private ApplicationContext applicationContext;
