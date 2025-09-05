7ae323c602f40c927306366a275186b348c4922e
xavelo <xavelo@gmail.com>
2025-09-05 19:23:38 +0200

    ListUsersPort

--- src/main/java/com/xavelo/template/render/api/application/service/UserService.java
+++ src/main/java/com/xavelo/template/render/api/application/service/UserService.java
@@ -1,6 +1,7 @@
 package com.xavelo.template.render.api.application.service;
 
 import com.xavelo.template.render.api.application.port.in.ListUsersUseCase;
+import com.xavelo.template.render.api.application.port.out.ListUsersPort;
 import com.xavelo.template.render.api.domain.User;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,15 @@ import java.util.List;
 @Service
 public class UserService implements ListUsersUseCase {
 
+    private ListUsersPort listUsersPort;
+
+    public UserService(ListUsersPort listUsersPort) {
+        this.listUsersPort = listUsersPort;
+    }
+
     @Override
     public List<User> listUsers() {
-        return List.of();
+        return listUsersPort.listUsers();
     }
 
 }
