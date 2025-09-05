cc7bc5f33c2e34c62d12634ba76fa7545b692eba
xavelo <xavelo@gmail.com>
2025-09-06 00:02:04 +0200

    fix compile

--- src/main/java/com/xavelo/template/render/api/application/service/UserService.java
+++ src/main/java/com/xavelo/template/render/api/application/service/UserService.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
-public class UserService implements ListUsersUseCase, CreateUserUseCase {
+public class UserService implements ListUsersUseCase, CreateUserUseCase, GetUserUseCase {
 
     private final ListUsersPort listUsersPort;
     private final CreateUserPort createUserPort;
