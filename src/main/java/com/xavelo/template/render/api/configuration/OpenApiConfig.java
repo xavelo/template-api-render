ea3855d29c18e5c0ba14c9660953da37529b403a
xavelo <xavelo@gmail.com>
2025-09-06 17:41:07 +0200

    Add Springdoc OpenAPI generation

---
+++ src/main/java/com/xavelo/template/render/api/configuration/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.xavelo.template.render.api.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Render API",
+                description = "Template API for render service"
+        )
+)
+public class OpenApiConfig {
+}
+
