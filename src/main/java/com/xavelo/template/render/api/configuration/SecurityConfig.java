1362c3c0b828b6fdae52d8420cd068736423eca6
xavelo <xavelo@gmail.com>
2025-09-06 14:24:37 +0200

    Disable Security

--- src/main/java/com/xavelo/template/render/api/configuration/SecurityConfig.java
+++ src/main/java/com/xavelo/template/render/api/configuration/SecurityConfig.java
@@ -2,32 +2,31 @@ package com.xavelo.template.render.api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
+@Configuration
 //@EnableWebSecurity
 public class SecurityConfig {
 
     private static final String TEMPLATE_API_SCOPE = "template-api";
+    /*
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authz -> authz
-                        /*
                         .requestMatchers("/api/ping/**").permitAll()
                         .requestMatchers("/api/user/**").permitAll()
                         .requestMatchers("/api/users/**").permitAll()
                         .requestMatchers("/api/latency/**").permitAll()
                         .requestMatchers("/api/secure/**").hasAuthority(TEMPLATE_API_SCOPE)
                         .anyRequest().authenticated()
-                        */
                         .anyRequest().permitAll()
                 )
                 //.oauth2ResourceServer(oauth2 -> oauth2.jwt());
         ;
 
         return http.build();
-    }
+    } */
 }
