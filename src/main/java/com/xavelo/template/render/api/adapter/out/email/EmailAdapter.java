3f04eccb5470acd0137f3069773f74a587758324
xavelo <xavelo@gmail.com>
2025-09-06 21:14:01 +0200

    feat: send emails via JavaMailSender

---
+++ src/main/java/com/xavelo/template/render/api/adapter/out/email/EmailAdapter.java
@@ -0,0 +1,30 @@
+package com.xavelo.template.render.api.adapter.out.email;
+
+import com.xavelo.template.render.api.application.port.out.SendEmailPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailAdapter implements SendEmailPort {
+    private static final Logger logger = LoggerFactory.getLogger(EmailAdapter.class);
+    private final JavaMailSender mailSender;
+
+    public EmailAdapter(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendEmail(String from, String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        mailSender.send(message);
+        logger.info("Sent email from {} to {} with subject '{}'", from, to, subject);
+    }
+}
+
