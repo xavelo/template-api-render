35790aacb240ce23d837ec4f7ffada07e6840f6b
xavelo <xavelo@gmail.com>
2025-09-06 14:08:44 +0200

    Disable JwtConfig

--- src/main/java/com/xavelo/template/render/api/configuration/JwtConfig.java
+++ src/main/java/com/xavelo/template/render/api/configuration/JwtConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
-@Configuration
+//@Configuration
 public class JwtConfig {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
