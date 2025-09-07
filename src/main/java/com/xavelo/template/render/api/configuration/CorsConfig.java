d7ba45da5a481cbbdedbb3b46820fff1d1530e0d
xavelo <xavelo@gmail.com>
2025-09-07 22:41:04 +0200

    CorsConfig

--- src/main/java/com/xavelo/template/render/api/configuration/CorsConfig.java
+++ src/main/java/com/xavelo/template/render/api/configuration/CorsConfig.java
@@ -6,10 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://authorizations-ui.onrender.com");
+                .allowedOrigins(
+                        "https://authorizations-ui.onrender.com",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
