cba0ca34cb3c6a6ddc49d1840bdb38cad4f72393
xavelo <xavelo@gmail.com>
2025-09-07 09:22:33 +0200

    feat: add migration to link authorizations to users

--- src/main/java/com/xavelo/template/render/api/application/port/in/CreateAuthorizationUseCase.java
+++ src/main/java/com/xavelo/template/render/api/application/port/in/CreateAuthorizationUseCase.java
@@ -2,8 +2,9 @@ package com.xavelo.template.render.api.application.port.in;
 
 import com.xavelo.template.render.api.domain.Authorization;
 import java.time.Instant;
+import java.util.UUID;
 
 public interface CreateAuthorizationUseCase {
-    Authorization createAuthorization(String title, String text, String status, String createdBy, String sentBy,
+    Authorization createAuthorization(String title, String text, String status, UUID createdBy, String sentBy,
                                       String approvedBy, Instant expiresAt);
 }
