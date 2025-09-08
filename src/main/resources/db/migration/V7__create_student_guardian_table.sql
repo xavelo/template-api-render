60d00300cf8dc7077bd9e0567b777d7b3f38d8d5
xavelo <xavelo@gmail.com>
2025-09-08 22:07:27 +0200

    refactor: consolidate flyway migrations

--- src/main/resources/db/migration/V7__create_student_guardian_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS public."student_guardian" (
-    student_id UUID NOT NULL,
-    guardian_id UUID NOT NULL,
-    PRIMARY KEY (student_id, guardian_id),
-    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES public."student"(id),
-    CONSTRAINT fk_guardian FOREIGN KEY (guardian_id) REFERENCES public."guardian"(id)
-);
