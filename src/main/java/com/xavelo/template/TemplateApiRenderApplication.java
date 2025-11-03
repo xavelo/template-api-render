2d9f27ce9cb77bd9bf9754fb3e255953c676954e
xavelo <xavelo@gmail.com>
2025-11-03 22:31:45 +0100

    Add startup datasource diagnostics logging

--- src/main/java/com/xavelo/template/TemplateApiRenderApplication.java
+++ src/main/java/com/xavelo/template/TemplateApiRenderApplication.java
@@ -1,17 +1,26 @@
 package com.xavelo.template;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.xavelo.template.render.api.configuration.DataSourceDiagnosticsApplicationListener;
 
 @SpringBootApplication
 @EnableScheduling
 public class TemplateApiRenderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TemplateApiRenderApplication.class, args);
-	}
+        private static final Logger log = LoggerFactory.getLogger(TemplateApiRenderApplication.class);
+
+        public static void main(String[] args) {
+                SpringApplication application = new SpringApplication(TemplateApiRenderApplication.class);
+                application.addListeners(new DataSourceDiagnosticsApplicationListener());
+                if (log.isDebugEnabled()) {
+                        log.debug("DataSource diagnostics listener registered for startup logging.");
+                }
+                application.run(args);
+        }
 
 }
