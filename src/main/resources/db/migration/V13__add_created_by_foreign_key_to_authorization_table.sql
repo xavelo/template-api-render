953529cc246caf4631f9b7d609c3636614eb83bf
xavelo <xavelo@gmail.com>
2025-11-04 13:40:26 +0100

    Remove Flyway migrations

--- src/main/resources/db/migration/V13__add_created_by_foreign_key_to_authorization_table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE public."authorization"
-    ALTER COLUMN created_by TYPE UUID USING created_by::uuid;
-
-ALTER TABLE public."authorization"
-    ADD CONSTRAINT fk_authorization_created_by FOREIGN KEY (created_by) REFERENCES public."user"(id);
