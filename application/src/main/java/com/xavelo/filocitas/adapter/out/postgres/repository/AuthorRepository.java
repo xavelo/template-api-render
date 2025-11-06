53396dd7d00bdc6407defe0e2d32103cc7777ab0
xavelo <xavelo@gmail.com>
2025-11-07 00:59:56 +0100

    Deduplicate author persistence when saving quotes

--- application/src/main/java/com/xavelo/filocitas/adapter/out/postgres/repository/AuthorRepository.java
+++ application/src/main/java/com/xavelo/filocitas/adapter/out/postgres/repository/AuthorRepository.java
@@ -3,7 +3,9 @@ package com.xavelo.filocitas.adapter.out.postgres.repository;
 import com.xavelo.filocitas.adapter.out.postgres.repository.entity.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AuthorRepository extends JpaRepository<AuthorEntity, UUID> {
+    Optional<AuthorEntity> findByNameIgnoreCase(String name);
 }
