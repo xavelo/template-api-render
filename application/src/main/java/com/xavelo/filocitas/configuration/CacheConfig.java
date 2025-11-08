6c9396717ce8cf943d2ff31f176b5123bd597eba
xavelo <xavelo@gmail.com>
2025-11-08 09:24:13 +0100

    Add Caffeine cache for author listing

---
+++ application/src/main/java/com/xavelo/filocitas/configuration/CacheConfig.java
@@ -0,0 +1,24 @@
+package com.xavelo.filocitas.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofMinutes(10))
+                .maximumSize(500));
+        return cacheManager;
+    }
+}
